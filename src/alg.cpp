0184211ecd626e52546f9e1241c0f22fbb0f2c94
Temidze <94217293+Temidze@users.noreply.github.com>
2022-03-17 21:01:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,19 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
+  int a = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int io = i+1; io < len; io++) {
+      if (arr[i] + arr[io] == value) {
+        a++;
+      }
+    }
+  }
+  return a;
+}
   return 0;
 }
 int countPairs2(int *arr, int len, int value) {
+  
   return 0;
 }
 int countPairs3(int *arr, int len, int value) {
